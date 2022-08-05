5649c74656a85ee78304e0f52782b1464001d39d
Chay <chongyu.zhu1997@gmail.com>
2022-08-05 18:16:02 -0400

    Add right limitation value to each joints based on action space and ad dof_index of each joint in post_reset

--- omniisaacgymenvs/robots/articulations/mycobot.py
+++ omniisaacgymenvs/robots/articulations/mycobot.py
@@ -22,7 +22,7 @@ class MyCobot(Robot):
         from omniisaacgymenvs.utils.usd_utils.create_instanceable_assets import convert_asset_instanceable
 
         # intanceable_asset_usd = Path(__file__).parent / "../assets/mycobot_urdf/mycobot_urdf.usd"
-        intanceable_asset_usd = Path(__file__).parent / "../assets/mycobot_with_instance.usd"
+        intanceable_asset_usd = Path(__file__).parent / "../assets/mycobot_original_with_instance.usd"
 
 
         if self._usd_path is None:
