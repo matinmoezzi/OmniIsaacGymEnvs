1f00885a32da800e8fd23a01441b7ffaff16bfc6
Chay <chongyu.zhu1997@gmail.com>
2022-10-06 10:25:51 -0400

    step function particial update and bug fix

--- omniisaacgymenvs/jetbot_to_mycobot/stable_baselines_example/env.py
+++ omniisaacgymenvs/jetbot_to_mycobot/stable_baselines_example/env.py
@@ -10,9 +10,11 @@ from symbol import parameters
 import gym
 from gym import spaces
 import numpy as np
+import os
 import math
 import carb
 import torch
+import pathlib
 
 
 class MyCobotEnv(gym.Env):
@@ -36,38 +38,19 @@ class MyCobotEnv(gym.Env):
         self._max_episode_length = max_episode_length
         self._steps_after_reset = int(rendering_dt / physics_dt)
         from omni.isaac.core import World
-        # from omni.isaac.wheeled_robots.robots import WheeledRobot
-        # from omni.isaac.wheeled_robots.controllers.differential_controller import DifferentialController
         from omni.isaac.core.objects import VisualCuboid
-        # from omni.isaac.core.utils.nucleus import get_assets_root_path
 
         self._my_world = World(physics_dt=physics_dt, rendering_dt=rendering_dt, stage_units_in_meters=1.0)
-        # self._my_world.scene.add_default_ground_plane()
-        # assets_root_path = get_assets_root_path()
-        # if assets_root_path is None:
-        #     carb.log_error("Could not find Isaac Sim assets folder")
-        #     return
-        # jetbot_asset_path = assets_root_path + "/Isaac/Robots/Jetbot/jetbot.usd"
-        # absolute path to the mycobot usd file
-        # mycobot_asset_path = "/home/chay/Desktop/Sim2Real_22Fall/mycobot_gripper_simplified_hands_deleted.usd"
-        mycobot_asset_path = "mycobot_gripper_simplified_hands_deleted.usd"
-        # self.jetbot = self._my_world.scene.add(
-        #     WheeledRobot(
-        #         prim_path="/jetbot",
-        #         name="my_jetbot",
-        #         wheel_dof_names=["left_wheel_joint", "right_wheel_joint"],
-        #         create_robot=True,
-        #         usd_path=jetbot_asset_path,
-        #         position=np.array([0, 0.0, 0.03]),
-        #         orientation=np.array([1.0, 0.0, 0.0, 0.0]),
-        #     )
-        # )
+
+        
+        usd_file_name = 'mycobot_gripper_simplified_hands_deleted.usd'
+        mycobot_asset_path = os.path.join(pathlib.Path(__file__).parent.resolve(),usd_file_name)
+
         from omni.isaac.core.robots.robot import Robot
         from omni.isaac.core.utils.stage import add_reference_to_stage
         add_reference_to_stage(usd_path=mycobot_asset_path, prim_path='/mycobot')
         mycobot = Robot(prim_path='/mycobot', name='MyCobot',scale=torch.tensor([1, 1, 1])*0.05)
 
-        # self.jetbot_controller = DifferentialController(name="simple_control", wheel_radius=0.0325, wheel_base=0.1125)
         self.goal = self._my_world.scene.add(
             VisualCuboid(
                 prim_path="/new_cube_1",
@@ -79,9 +62,9 @@ class MyCobotEnv(gym.Env):
         )
 
         from omni.isaac.core.articulations import ArticulationView
-        from omni.isaac.core.prims import GeometryPrim
+        # from omni.isaac.core.prims import GeometryPrim
         self._mycobot = ArticulationView(prim_paths_expr="/mycobot",name='mycobot_view')
-        self.tip = ArticulationView()
+        self._tip = ArticulationView(prim_paths_expr="/mycobot/gripper_shell/collisions")
 
         from omni.isaac.core.utils.stage import get_current_stage
         from pxr import UsdLux
@@ -97,7 +80,8 @@ class MyCobotEnv(gym.Env):
         self.num_dof = 6
 
         self.action_space = spaces.Box(low=-1, high=1.0, shape=(6,), dtype=np.float32)
-        self.observation_space = spaces.Box(low=float("inf"), high=float("inf"), shape=(16,), dtype=np.float32)
+        # Vision Based observation_space, which need to match the method get_observation return
+        self.observation_space = spaces.Box(low=int(0), high=int(255), shape=(64,64,3), dtype=np.intc)
 
         self.max_velocity = 1
         self.max_angular_velocity = math.pi
@@ -107,26 +91,30 @@ class MyCobotEnv(gym.Env):
     def get_dt(self):
         return self._dt
 
-    def step(self, action):
-        previous_jetbot_position, _ = self.jetbot.get_world_pose()
-        # action forward velocity , angular velocity on [-1, 1]
-        raw_forward = action[0]
-        raw_angular = action[1]
+    def step(self, action): # action is produced by policy (embeded)
+        previous_mycobot_tip_position, _ = self._tip.get_world_pose()
+        # # previous_jetbot_position, _ = self.jetbot.get_world_pose()
+        # # action forward velocity , angular velocity on [-1, 1]
+        # raw_forward = action[0]
+        # raw_angular = action[1]
 
-        # we want to force the jetbot to always drive forward
-        # so we transform to [0,1].  we also scale by our max velocity
-        forward = (raw_forward + 1.0) / 2.0
-        forward_velocity = forward * self.max_velocity
+        # # we want to force the jetbot to always drive forward
+        # # so we transform to [0,1].  we also scale by our max velocity
+        # forward = (raw_forward + 1.0) / 2.0
+        # forward_velocity = forward * self.max_velocity
 
-        # we scale the angular, but leave it on [-1,1] so the
-        # jetbot can remain an ambiturner.
-        angular_velocity = raw_angular * self.max_angular_velocity
+        # # we scale the angular, but leave it on [-1,1] so the
+        # # jetbot can remain an ambiturner.
+        # angular_velocity = raw_angular * self.max_angular_velocity
 
-        # we apply our actions to the jetbot
+        # # we apply our actions to the jetbot
+        # for i in range(self._skip_frame):
+        #     self.jetbot.apply_wheel_actions(
+        #         self.jetbot_controller.forward(command=[forward_velocity, angular_velocity])
+        #     )
+        #     self._my_world.step(render=False)
         for i in range(self._skip_frame):
-            self.jetbot.apply_wheel_actions(
-                self.jetbot_controller.forward(command=[forward_velocity, angular_velocity])
-            )
+            self._mycobot.set_joint_positions(positions=torch.tensor())
             self._my_world.step(render=False)
 
         observations = self.get_observations()
@@ -135,9 +123,9 @@ class MyCobotEnv(gym.Env):
         if self._my_world.current_time_step_index - self._steps_after_reset >= self._max_episode_length:
             done = True
         goal_world_position, _ = self.goal.get_world_pose()
-        current_jetbot_position, _ = self.jetbot.get_world_pose()
-        previous_dist_to_goal = np.linalg.norm(goal_world_position - previous_jetbot_position)
-        current_dist_to_goal = np.linalg.norm(goal_world_position - current_jetbot_position)
+        current_mycobot_tip_position, _ = self._tip.get_world_pose()
+        previous_dist_to_goal = np.linalg.norm(goal_world_position - previous_mycobot_tip_position)
+        current_dist_to_goal = np.linalg.norm(goal_world_position - current_mycobot_tip_position)
         reward = previous_dist_to_goal - current_dist_to_goal
         if current_dist_to_goal < 0.1:
             done = True
@@ -155,19 +143,6 @@ class MyCobotEnv(gym.Env):
 
     def get_observations(self):
         self._my_world.render()
-        # jetbot_world_position, jetbot_world_orientation = self.jetbot.get_world_pose()
-        # jetbot_linear_velocity = self.jetbot.get_linear_velocity()
-        # jetbot_angular_velocity = self.jetbot.get_angular_velocity()
-        # goal_world_position, _ = self.goal.get_world_pose()
-        # return np.concatenate(
-        #     [
-        #         jetbot_world_position,
-        #         jetbot_world_orientation,
-        #         jetbot_linear_velocity,
-        #         jetbot_angular_velocity,
-        #         goal_world_position,
-        #     ]
-        # )
         mycobot_angular_velocity = self._mycobot.get_angular_velocities()
         mycobot_angle_postion = self._mycobot.get_world_poses()
 
@@ -198,7 +173,7 @@ class MyCobotEnv(gym.Env):
         )
         viewport_window = omni.kit.viewport_legacy.get_viewport_interface().get_viewport_window(viewport_handle)
         viewport_window.set_active_camera("/mycobot/camera_flange/rgb_camera")
-        viewport_window.set_texture_resolution(128, 128)
+        viewport_window.set_texture_resolution(64, 64)
         # viewport_window.set_camera_position("/OmniverseKit_Persp",100,100,60,True)
         # viewport_window.set_camera_target("/OmniverseKit_Persp",0,0,0,True)
         viewport_window.set_window_pos(1000, 400)
